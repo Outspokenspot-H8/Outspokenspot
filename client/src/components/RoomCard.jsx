d6bfc71529aa5f56d6dc9a6e0036fb97bbbafe37
rendramakmur <rendra1795@gmail.com>
2021-04-20 00:20:22 +0700

    Minor fix on random avatars size

--- client/src/components/RoomCard.jsx
+++ client/src/components/RoomCard.jsx
@@ -11,7 +11,7 @@ export default function RoomCard({user}) {
   return (
     <div class="d-flex flex-row m-3 card" style={{width: "23vw"}}>
       <div class="flex-fill d-flex flex-column align-items-center justify-content-center">
-        <img class="my-3" src={ava} alt="Card image cap" />
+        <img class="my-3 rounded-3" src={ava} alt="Card image cap" style={{width: "100px"}} />
       </div>
       <div class="flex-fill d-flex justify-content-center align-items-center flex-column text-center">
         <h3>{user.username}</h3>
