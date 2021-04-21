81b22399c99ea6321632cd989b412c2fed61bd88
rendramakmur <rendra1795@gmail.com>
2021-04-21 09:32:43 +0700

    Minor fix in PlayerCard peer?.peer

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -3,13 +3,13 @@ import Video from './Video';
 
 export default function PlayerCard({peer, turn, idx}) {
   return (
-    <div className={`d-flex flex-column m-3 card card-${idx}`} style={{width: "21rem", height: "40%"}} id={turn === peer.user.username ? "border-active" : ""}>
+    <div className={`d-flex flex-column m-3 card card-${idx}`} style={{width: "21rem", height: "40%"}} id={turn === peer?.user?.username ? "border-active" : ""}>
       <div className="flex-fill align-items-start d-flex justify-content-center bg-secondary" style={{height: "60%"}}>
-        <Video peer={peer.peer} ></Video>
+        <Video peer={peer?.peer} ></Video>
       </div>
       <div className="flex-fill d-flex justify-content-around align-items-center flex-row text-center bg-light" style={{zIndex: "2"}}>
         <h3>{peer.user.username}</h3>
-        <p style={{margin: '0px'}}>{peer.user.location}</p>
+        <p style={{margin: '0px'}}>{peer?.user?.location}</p>
       </div>
     </div>
   )
